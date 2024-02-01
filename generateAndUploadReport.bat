22cce9027e600f1e5a3733746e3adb3250c41dec
ravittcglobalcom <154219351+ravittcglobalcom@users.noreply.github.com>
2024-02-01 14:11:03 +0530

    Test1

--- generateAndUploadReport.bat
+++ generateAndUploadReport.bat
@@ -6,8 +6,8 @@ SET TESTWISE_COVERAGE_REPORT=""C:\Users\DELL\Downloads\teamscale-jacoco-agent\te
 SET TEAMSCALE_URL=http://54.253.207.240:8080/
 SET TEAMSCALE_PROJECT=ravi
 SET TEAMSCALE_USER=admin
-SET TEAMSCALE_ACCESS_TOKEN=o0jXPd2NuAdsJq0gkocWVbxLc072DZCx
+SET TEAMSCALE_ACCESS_TOKEN=HSUOD9bWNB7MJI3IKTclLl6vbQDgYFpH
 SET TEAMSCALE_PARTITION=Tosca
 SET TEAMSCALE_COMMIT=main:%TEAMSCALE_REVISION% :: TODO In production, this should point to the git commit timestamp
 
-curl --request POST --user %TEAMSCALE_USER%:%TEAMSCALE_ACCESS_TOKEN% --form "report=@%TESTWISE_COVERAGE_REPORT%" "%TEAMSCALE_URL%api/projects/%TEAMSCALE_PROJECT%/external-analysis/session/auto-create/report?format=TESTWISE_COVERAGE&partition=Tosca%2Tests&message=Unit%2Test%2Coverage"
+curl --request POST --user %TEAMSCALE_USER%:%TEAMSCALE_ACCESS_TOKEN% --form "report=@%TESTWISE_COVERAGE_REPORT%" "%TEAMSCALE_URL%api/projects/%TEAMSCALE_PROJECT%/external-analysis/session/auto-create/report?format=TESTWISE_COVERAGE&partition=Tosca%2Tests&message=Tosca%2Test%2Coverage"
